9083c3e682401f2f184c01c94417d326f21a01e6
imancrrzii <imancarrazi777@gmail.com>
2024-01-31 21:04:35 +0700

    Create layout views for sidebar, headers, footers and content

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="nk-footer">
+    <div class="container-fluid">
+        <div class="nk-footer-wrap">
+            <div class="nk-footer-copyright text-center"> &copy; 2023 Sriwijaya Medical Assistance
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,34 @@
+<div class="nk-sidebar nk-sidebar-fixed is-light " data-content="sidebarMenu">
+    <div class="nk-sidebar-element nk-sidebar-head">
+        <div class="nk-sidebar-brand">
+            <a href="" class="logo-link nk-sidebar-logo">
+                <img class="logo-light logo-img" src=""
+                    srcset="./images/logo2x.png 2x" alt="logo">
+                <img class="logo-dark logo-img" src=""
+                    srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                <img class="logo-small logo-img logo-img-small" src=""
+                    srcset="./images/logo-small2x.png 2x" alt="logo-small">
+            </a>
+        </div>
+        <div class="nk-menu-trigger me-n2">
+            <a href="#" class="nk-nav-toggle nk-quick-nav-icon d-xl-none" data-target="sidebarMenu"><em
+                    class="icon ni ni-arrow-left"></em></a>
+            <a href="#" class="nk-nav-compact nk-quick-nav-icon d-none d-xl-inline-flex"
+                data-target="sidebarMenu"><em class="icon ni ni-menu"></em></a>
+        </div>
+    </div>
+    <div class="nk-sidebar-element">
+        <div class="nk-sidebar-content">
+            <div class="nk-sidebar-menu" data-simplebar>
+                <ul class="nk-menu">
+                    <li class="nk-menu-item">
+                        <a href="" class="nk-menu-link">
+                            <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
+                            <span class="nk-menu-text"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
